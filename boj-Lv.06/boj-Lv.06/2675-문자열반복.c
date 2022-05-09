1df62719311832fe0de59124c8a8620f9364edd3
YJY1220 <jiyeyu1220@gmail.com>
2022-05-10 05:56:56 +0900

    complete boj-Lv.06

---
+++ boj-Lv.06/boj-Lv.06/2675-문자열반복.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int T, R;
+    char S[21];
+
+    scanf("%d", &T);
+    //T��ŭ�� �׽�Ʈ���̽�
+    while (T--) {
+        scanf("%d %s", &R, S);
+
+        //����� ����ϱ� ���� �ݺ���
+        for (int i = 0; i < strlen(S); i++) {
+            for (int j = 0; j < R; j++)
+                printf("%c", S[i]);
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
